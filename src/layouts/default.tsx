e81e0d9694bba6f2b2fd8b134585136a9681a29f
FjrREPO <jossfajar21@gmail.com>
2025-01-27 22:22:16 +0700

    refactor layout styles to improve responsiveness and background handling

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -13,7 +13,7 @@ export default function DefaultLayout({
       <div className="max-w-[2000px] w-full overflow-y-auto overflow-x-hidden">
         <div className={clsx(
           "flex min-h-[calc(100vh-20px)] sm:min-h-[calc(100vh-40px)] flex-col rounded-3xl mx-2.5 sm:mx-[30px] my-2.5 sm:my-5 z-20 top-0 left-0 right-0 bottom-0 relative w-[calc(100vw - 9px] max-w-[2000px] 4xl:mx-auto layout-background-image border-2 border-gray-500 overflow-y-auto",
-          "bg-herosminvert dark:bg-herosm bg-cover bg-no-repeat bg-center"
+          "bg-cover bg-no-repeat bg-center"
         )}>
           <div className="w-full h-full backdrop-blur-lg backdrop-contrast-125 dark:backdrop-brightness-50 p-5 sm:p-10 flex flex-col flex-1">
             <Navbar />
